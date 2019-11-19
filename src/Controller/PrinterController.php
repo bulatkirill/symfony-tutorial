772e2afddc516bb02d99617be6852f0a21680a33
bulatkyr <bulatkyr@fit.cvut.cz>
2019-11-19 22:01:39 +0100

    KBU: added encore

--- src/Controller/PrinterController.php
+++ src/Controller/PrinterController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Entity\Printer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PrinterController extends AbstractController
@@ -17,7 +18,7 @@ class PrinterController extends AbstractController
     {
         $result = array();
         for ($i = 0; $i < 10; $i++) {
-            array_push($result, new Printer($i, 'printer' . $i, "no note yet"));
+            $result[$i] = new Printer($i, 'printer' . $i, "no note yet");
         }
         return $result;
     }
@@ -25,14 +26,31 @@ class PrinterController extends AbstractController
     /**
      * @Route("/printers", name="printer_list")
      */
-    public function printers()
+    public function index()
     {
         if (count($this->printers) === 0) {
             $this->printers = $this->createTestPrinters();
         }
-        return $this->render('printers.html.twig', [
+        return $this->render('printer/index.html.twig', [
             'printers' => $this->printers,
         ]);
+    }
+
+    /**
+     * @Route("/printers/{id}", name="printer_detail", methods={"GET", "HEAD"}, requirements={"id"="\d+"})
+     * @param int $id
+     * @return Response
+     */
+    public function detail(int $id)
+    {
+        $this->printers = $this->createTestPrinters();
+        // is_null check
+        if (!is_null($this->printers[$id])) {
+            return $this->render('printer/detail.html.twig', [
+                'printer' => $this->printers[$id],
+            ]);
+        }
+        return $this->render('error.html.twig');
     }
 
 }
